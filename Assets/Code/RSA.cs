5e8f5261b555209ec3515e321d42f31ef3954b5a
Corey Wolff <jesterswilde@gmail.com>
2020-08-25 16:22:45 -0700

    Got RSA Working

--- Assets/Code/RSA.cs
+++ Assets/Code/RSA.cs
@@ -69,34 +69,131 @@ public class RSA : MonoBehaviour
     }
 
 
-    public static (BigInteger, BigInteger) GenerateKeys(int size)
+    public static (BigInteger, BigInteger) GenerateKeys(int size = 64)
     {
-
-        var rng = new RNGCryptoServiceProvider();
-        byte[] privBytes = new byte[size / 8];
-        rng.GetBytes(privBytes);
-        BigInteger privKey = new BigInteger(privBytes);
-        byte[] pubBytes = new byte[size / 8];
-        rng.GetBytes(pubBytes);
-        BigInteger pubKey = new BigInteger(pubBytes);
-        return (BigInteger.Abs(pubKey), BigInteger.Abs(privKey));
+        var p = BigIntegerExtensions.GeneratePrime(16);
+        var q = BigIntegerExtensions.GeneratePrime(16);
+        var n = p * q;
+        var phiOfN = (p - 1) * (q - 1);
+        var pubKey = PickPubKey(phiOfN);
+        var privKey = pubKey.ModInverse(phiOfN); 
+        BigInteger message = 123;
+        var cipher = BigInteger.ModPow(message, pubKey, n);
+        var decrypted = BigInteger.ModPow(cipher, privKey, n);
+        return (pubKey, privKey); 
+        //var rng = new RNGCryptoServiceProvider();
+        //byte[] privBytes = new byte[size / 8];
+        //rng.GetBytes(privBytes);
+        //BigInteger privKey = new BigInteger(privBytes);
+        //byte[] pubBytes = new byte[size / 8];
+        //rng.GetBytes(pubBytes);
+        //BigInteger pubKey = new BigInteger(pubBytes);
+        //return (BigInteger.Abs(pubKey), BigInteger.Abs(privKey));
+    }
+    static BigInteger PickPubKey(BigInteger phiOfN)
+    {
+        while (true)
+        {
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                var potentialKey = BigIntegerExtensions.RandomInRange(rng, 3, phiOfN - 1);
+                if (BigInteger.GreatestCommonDivisor(potentialKey, phiOfN) == 1)
+                {
+                    return potentialKey; 
+                }
+            }
+        }
     }
     
+
 }
 
 
 public static class BigIntegerExtensions
 {
-    public static BigInteger GeneratePrime(int size = 64){
+    public static BigInteger ModInverse(this BigInteger a, BigInteger m)
+    {
+        if (m == 1) return 0;
+        BigInteger m0 = m;
+        (BigInteger x, BigInteger y) = (1, 0);
+
+        while (a > 1)
+        {
+            BigInteger q = a / m;
+            (a, m) = (m, a % m);
+            (x, y) = (y, x - q * y);
+        }
+        return x < 0 ? x + m0 : x;
+    }
+    public static BigInteger GeneratePrime(int size = 64)
+    {
         var rng = new RNGCryptoServiceProvider();
-        while(true){
+        while (true)
+        {
             byte[] randBytes = new byte[size / 8];
             rng.GetBytes(randBytes);
             BigInteger rand = new BigInteger(randBytes);
-            if(rand.IsProbablePrime(10)){
-                return rand; 
+            if (rand.IsProbablePrime(10))
+            {
+                return rand;
             }
         }
+    }
+    public static BigInteger RandomInRange(RandomNumberGenerator rng, BigInteger min, BigInteger max)
+    {
+        if (min > max)
+        {
+            var buff = min;
+            min = max;
+            max = buff;
+        }
+
+        // offset to set min = 0
+        BigInteger offset = -min;
+        min = 0;
+        max += offset;
+
+        var value = randomInRangeFromZeroToPositive(rng, max) - offset;
+        return value;
+    }
+
+    private static BigInteger randomInRangeFromZeroToPositive(RandomNumberGenerator rng, BigInteger max)
+    {
+        BigInteger value;
+        var bytes = max.ToByteArray();
+
+        // count how many bits of the most significant byte are 0
+        // NOTE: sign bit is always 0 because `max` must always be positive
+        byte zeroBitsMask = 0b00000000;
+
+        var mostSignificantByte = bytes[bytes.Length - 1];
+
+        // we try to set to 0 as many bits as there are in the most significant byte, starting from the left (most significant bits first)
+        // NOTE: `i` starts from 7 because the sign bit is always 0
+        for (var i = 7; i >= 0; i--)
+        {
+            // we keep iterating until we find the most significant non-0 bit
+            if ((mostSignificantByte & (0b1 << i)) != 0)
+            {
+                var zeroBits = 7 - i;
+                zeroBitsMask = (byte)(0b11111111 >> zeroBits);
+                break;
+            }
+        }
+
+        do
+        {
+            rng.GetBytes(bytes);
+
+            // set most significant bits to 0 (because `value > max` if any of these bits is 1)
+            bytes[bytes.Length - 1] &= zeroBitsMask;
+
+            value = new BigInteger(bytes);
+
+            // `value > max` 50% of the times, in which case the fastest way to keep the distribution uniform is to try again
+        } while (value > max);
+
+        return value;
     }
     public static bool IsProbablePrime(this BigInteger source, int certainty)
     {
